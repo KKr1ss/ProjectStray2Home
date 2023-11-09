ce1472eb82cd7951f98d32b448deee5a26bb7cc8
KKr1ss <k.krisztoferbela@gmail.com>
2023-11-09 09:35:27 +0100

    Profil UI redesign

--- Client/ProjectStray2Home/src/app/user/profile/profile.component.ts
+++ Client/ProjectStray2Home/src/app/user/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { catchError, map, Observable, of} from 'rxjs';
+import { BreakpointService } from '../../common/breakpoint.service';
 import { ImageDownloadService } from '../../common/image-download.service';
 import { AssetUrls } from '../../shared/asset-urls';
 import { UserProfile } from '../common/models/user-profile';
@@ -16,10 +17,13 @@ export class ProfileComponent implements OnInit {
   profile$?: Observable<UserProfile>
   username!: string
 
+  isTablet$?: Observable<boolean>
+
   constructor(
     private userService: UserService,
     private activatedRoute: ActivatedRoute,
-    private imageDownloadService: ImageDownloadService) {
+    private imageDownloadService: ImageDownloadService,
+    private breakpointService: BreakpointService) {
   }
 
   ngOnInit(): void {
@@ -40,19 +44,6 @@ export class ProfileComponent implements OnInit {
       }))
     })
 
-    //let usernameParam = this.activatedRoute.snapshot.paramMap.get('username');
-    //this.username = usernameParam!
-    //this.profile$ = this.userService.getProfileDetails(this.username).pipe(map(x => {
-    //  x.sex = (x.sex == "Male" ? "Férfi" : "Nő")
-    //  x.image$ = this.imageDownloadService.getUserProfileImage(x.id).pipe(map(y => {
-    //    return this.imageDownloadService.convertBlobToSafeUrl(y)
-    //  }),
-    //    catchError((err: HttpErrorResponse) => {
-    //      return of(AssetUrls.defaultUserImageUrl);
-    //    })
-    //  )
-    //  return x;
-    //}))
-    
+     this.isTablet$ = this.breakpointService.isTablet();
   }
 }
